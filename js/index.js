728de15549d6955d3de574c106967dd375df55f8
alisherxalilov <alisherxalilov23@gmail.com>
2022-12-02 08:00:46 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// armiya
+
+let boy=prompt("bo/'yingizni kiriting");
+let vazn=prompt("vazningizni kiriting");
+if(boy>165 || vazn>60) alert("armiyaga qa'bul qilindingiz");
+else alert("armiyaga qa'bul qilinmadingiz")
+
+
+
+let c=prompt("hozirgi vaqtni kiriting fr(0800")
+if(c<08){
+    alert("darsga kechikmadingiz")
+}
+else{
+    alert("darsga kechikdingiz")
+}
